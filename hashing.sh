3b8274a2aa17f6481abc6710d794c0bcd3381c8e
Yann Duval <me@yannduval.de>
2018-07-15 15:47:15 +0200

    using the globally available commands

--- hashing.sh
+++ hashing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-HASH=`./node_modules/.bin/rev-hash --rename ./static/css/learn-love-save.css | awk '{print $1}'`
+HASH=`rev-hash --rename ./static/css/learn-love-save.css | awk '{print $1}'`
 
-HASH_JS=`./node_modules/.bin/rev-hash --rename ./static/js/learn-love-save.js | awk '{print $1}'`
+HASH_JS=`rev-hash --rename ./static/js/learn-love-save.js | awk '{print $1}'`
 
 echo "{ \"css/learn-love-save.css\": \"/css/learn-love-save-$HASH.css\", \"js/learn-love-save.js\": \"/js/learn-love-save-$HASH_JS.js\" }" > ./data/assets.json
