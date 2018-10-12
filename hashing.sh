0b1e3aa1c601f9bbee188b015dcdeba505595483
Yann Duval <me@yannduval.de>
2018-10-12 21:23:38 +0200

    Fixing creattion of SRI's

--- hashing.sh
+++ hashing.sh
@@ -12,5 +12,5 @@ SRI=`shasum -b -a ${SRI_FUNCTION} ./static/css/learn-love-save-$HASH.css | xxd -
 HASH_JS=`rev-hash --rename ./static/js/learn-love-save.js | awk '{print $1}'`
 SRI_JS=`shasum -b -a ${SRI_FUNCTION} ./static/js/learn-love-save-$HASH_JS.js | xxd -r -p | base64`
 
-echo "{ \"filename\": \"/css/learn-love-save-$HASH.css\", \"sri\": \"sha${SRI_FUNCTION}-$SRI\" }" > ./data/assets/css/lls.json
-echo "{ \"filename\": \"/js/learn-love-save-$HASH_JS.js\", \"sri\": \"sha${SRI_FUNCTION}-$SRI_JS\" }" > ./data/assets/js/lls.json
+echo "{ \"filename\": \"/css/learn-love-save-$HASH.css\", \"sri${SRI_FUNCTION}\": \"sha${SRI_FUNCTION}-$SRI\" }" > ./data/assets/css/lls.json
+echo "{ \"filename\": \"/js/learn-love-save-$HASH_JS.js\", \"sri${SRI_FUNCTION}\": \"sha${SRI_FUNCTION}-$SRI_JS\" }" > ./data/assets/js/lls.json
