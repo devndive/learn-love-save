fb13401777ee7d8787937bd9ccbeb213e3d6006a
Yann Duval <me@yannduval.de>
2019-03-31 15:32:23 +0200

    Updating to bootstrap 4

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 #HUGO_DESTINATION="${HUGO_DESTINATION}:=/output}"
 PWD=`pwd`
@@ -14,10 +15,10 @@ mkdir -p ./data/assets/css/
 mkdir -p ./data/assets/js/
 
 HASH=`${HUGO} rev-hash --rename ./static/css/learn-love-save.css | awk '{print $1}'`
-SRI_384=`${HUGO} shasum -b -a 384 ./static/css/learn-love-save-$HASH.css | xxd -r -p | base64`
+SRI_384=`${HUGO} cat ./static/css/learn-love-save-$HASH.css | openssl dgst -sha384 -binary | openssl base64 -A`
 
 HASH_JS=`${HUGO} rev-hash --rename ./static/js/learn-love-save.js | awk '{print $1}'`
-SRI_384_JS=`${HUGO} shasum -b -a 384 ./static/js/learn-love-save-$HASH_JS.js | xxd -r -p | base64`
+SRI_384_JS=`${HUGO} cat ./static/js/learn-love-save-$HASH_JS.js | openssl dgst -sha384 -binary | openssl base64 -A`
 
 echo "{ \"filename\": \"/css/learn-love-save-$HASH.css\", \"sri384\": \"sha384-$SRI_384\" }" > ./data/assets/css/lls.json
 echo "{ \"filename\": \"/js/learn-love-save-$HASH_JS.js\", \"sri384\": \"sha384-$SRI_384_JS\" }" > ./data/assets/js/lls.json
